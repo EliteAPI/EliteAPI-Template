6da26f6f78358ead5ffd15769e1c9cc50dde2e68
Somfic <somfic@gmail.com>
2020-12-29 09:53:34 +0100

    Created template

---
+++ TemplateSolution/TemplateProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using EliteAPI;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Somfic.Logging.Console;
+using Somfic.Logging.Console.Themes;
+
+using System.Threading.Tasks;
+
+using TemplateProject.Modules;
+
+// Build the host for dependency injection
+var host = Host.CreateDefaultBuilder()
+    .ConfigureLogging((context, logger) =>
+    {
+        logger.ClearProviders();
+        logger.SetMinimumLevel(LogLevel.Trace);
+        logger.AddPrettyConsole(ConsoleThemes.Code);
+    })
+
+    .ConfigureServices((context, service) =>
+    {
+        // Add EliteAPI's services to the dependency injection system
+        service.AddEliteAPI(config =>
+        {
+            // Add our ChatModule class to the event pipeline
+            config.AddEventModule<ChatModule>();
+        });
+    })
+
+    .Build();
+
+// Create an instance of our Core class
+var core = ActivatorUtilities.CreateInstance<TemplateProject.Core>(host.Services);
+
+// Execute the Run method inside our Core class
+await core.Run();
+
+// Run forever
+await Task.Delay(-1);
